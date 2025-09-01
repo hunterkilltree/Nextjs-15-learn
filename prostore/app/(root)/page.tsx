6056432852c2aeb2f679707bb0e5eb33971f4446
hunterkilltree <snguoila@yahoo.com>
2025-09-01 11:10:36 +0700

    Add product list

--- prostore/app/(root)/page.tsx
+++ prostore/app/(root)/page.tsx
@@ -1,10 +1,19 @@
 // const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 // const HomePage = async () => {
 //   await delay(5000);
 //   return <>Prostore</>;
 // };
 
-const HomePage = () => <>Prostore</>;
+const HomePage = () => {
+  return (
+    <div>
+      <ProductList data={sampleData.products} title="New Arrivals" limit={4} />
+    </div>
+  );
+};
 
 export default HomePage;
